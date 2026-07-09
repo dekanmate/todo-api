888320389f8d46e0a22a48420ad1e900a9d67d2a
Máté D. <dekan.mate@gmail.com>
2026-07-09 11:39:23 +0200

    Added logger for exceptions

--- ToDoApi/Middleware/ExceptionMiddleware.cs
+++ ToDoApi/Middleware/ExceptionMiddleware.cs
@@ -6,10 +6,12 @@ namespace ToDoApi.Middleware;
 public class ExceptionMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
 
-    public ExceptionMiddleware(RequestDelegate next)
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context)
@@ -20,6 +22,8 @@ public class ExceptionMiddleware
         }
         catch (Exception ex)
         {
+            _logger.LogError(ex, "An unhandled exception occurred.");
+
             context.Response.ContentType = "application/json";
 
             int statusCode;
